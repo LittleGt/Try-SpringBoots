0b600ea9d83a1f9070052c88774ea8f489e87970
Litoru <119419681+LittleGt@users.noreply.github.com>
2023-04-19 02:53:18 +0700

    u

--- src/main/java/com/example/Test/Test/model/Cus.java
+++ src/main/java/com/example/Test/Test/model/Cus.java
@@ -1,12 +1,6 @@
 package com.example.Test.Test.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-
-import jakarta.persistence.Column;
-import jakarta.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "customer")
